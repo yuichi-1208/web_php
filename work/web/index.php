60ad39cff802cc65818b154b5c356ccf60e2e19f
yuichi oyama <yuichi19991208@gmail.com>
2021-02-15 23:32:34 +0900

    拡張子を変更

---
+++ work/web/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$today = date('Y-m-d H:i:s l');
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <p>Hello, PHP!</p>
+  <p>Today: <?php echo date('Y-m-d H:i:s l'); ?></p>
+  <p>Today: <?= date('Y-m-d H:i:s l'); ?></p>
+  <p>Today: <?= $today; ?></p>
+</body>
+</html>
