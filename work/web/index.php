70f8660fc5ff8681195afc7082a541db8959014a
yuichi oyama <yuichi19991208@gmail.com>
2021-02-15 23:39:09 +0900

    文字実態参照

--- work/web/index.php
+++ work/web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 $today = date('Y-m-d H:i:s l');
+$name = 'Taro <script>alert(1);</script>';
 
 ?>
 <!DOCTYPE html>
@@ -14,5 +15,6 @@ $today = date('Y-m-d H:i:s l');
   <p>Today: <?php echo date('Y-m-d H:i:s l'); ?></p>
   <p>Today: <?= date('Y-m-d H:i:s l'); ?></p>
   <p>Today: <?= $today; ?></p>
+  <p>Hello, <?= htmlspecialchars($name, ENT_QUOTES, 'UTF-8'); ?></p>
 </body>
 </html>
