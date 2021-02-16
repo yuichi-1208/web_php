50f074930b87badc289d71c28ab2d65f419199fd
yuichi oyama <yuichi19991208@gmail.com>
2021-02-16 23:04:53 +0900

    フォームの入力内容によって条件分岐

--- work/web/result.php
+++ work/web/result.php
@@ -3,7 +3,9 @@
 require('../app/functions.php');
 
 //フォームから送られてきた値を受け取るにはfilter_input
-$message = filter_input(INPUT_GET, 'message');
+//trimは空白を削除する
+$message = trim(filter_input(INPUT_GET, 'message'));
+$message = $message !== '' ? $message : '...';
 $username = filter_input(INPUT_GET, 'username');
 
 include('../app/_parts/_header.php');
